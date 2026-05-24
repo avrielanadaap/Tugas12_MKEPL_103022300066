2fd6e7b6e9143a85022180646e86b51fcc0a42d6
avrielanada <avrielanadaamaraputri@gmail.com>
2026-05-24 20:14:06 +0700

    simulasi bad code

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,7 +1,18 @@
 package src.main.java;
 
 public class Counter {
+	
+ public void badCode() {
+    int a = 1;
+    int b = 2;
+    int c = a + b;
+    System.out.println(c);
 
+    int x = 1;
+    int y = 2;
+    int z = x + y;
+    System.out.println(z);
+}
 	private int count;
 	
 	public Counter() {
